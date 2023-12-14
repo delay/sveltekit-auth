1d039606e3d2ff7f6192c3bdb6fce6262bc2bb4c
Jeff McMorris <delay@pobox.com>
2023-12-14 11:13:11 -0600

    update

--- src/lib/config/constants.ts
+++ src/lib/config/constants.ts
@@ -1,5 +1,5 @@
 import { dev } from '$app/environment';
-export const BASE_URL = dev ? 'http://localhost:5173' : 'https://sveltekit-auth.uv-ray.com';
+export const BASE_URL = dev ? 'http://localhost:5173' : 'https://sveltekit-auth-alpha.vercel.app/';
 export const APP_NAME = 'Sveltekit Auth';
 export const CONTACT_EMAIL = 'yourname@email.com';
 export const DOMAIN = 'sveltekit-auth.uv-ray.com';
