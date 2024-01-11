bb0bc2ca710b01e5ce6340a823f3cec6759ceb56
Jeff McMorris <delay@pobox.com>
2024-01-11 13:29:46 -0600

    tried integrating with reirect on signout  still not showing toast.

--- src/routes/auth/sign-out/+page.server.ts
+++ src/routes/auth/sign-out/+page.server.ts
@@ -1,4 +1,24 @@
 import { auth } from '$lib/server/lucia';
+import { redirect } from 'sveltekit-flash-message/server';
+
+export const actions = {
+	default: async (event) => {
+		const session = await event.locals.auth.validate();
+		if (!session) {
+			redirect(302, '/auth/sign-in');
+		}
+		
+		await auth.invalidateSession(session.sessionId); // invalidate session
+		event.locals.auth.setSession(null); // remove cookie
+		const message = { type: 'success', message: 'Logged out' } as const;
+		redirect(302, '/auth/sign-in', message, event.cookies);
+		
+	}
+};
+
+//toast also doesn't work this way.
+/*
+import { auth } from '$lib/server/lucia';
 import { setFlash } from 'sveltekit-flash-message/server';
 import { redirect } from '@sveltejs/kit';
 export const actions = {
@@ -15,3 +35,4 @@ export const actions = {
 		
 	}
 };
+*/
